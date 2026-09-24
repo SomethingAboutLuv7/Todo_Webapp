7a2feb5d0d90305fb2c9bb82f5f9bc29be61a974
SomethingAboutLuv7 <yeagere329@gmail.com>
2026-09-24 20:49:56 +0545

    Update ALLOWED_HOSTS

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-old-key-as-fallback')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['luvchau.pythonanywhere.com', '.pythonanywhere.com']
 
 # Application definition
 
